fe51c9be6cce407e92d5730cdabb99ae747ce229
rajthilakam <rajthilakam@gmail.com>
2021-07-09 23:12:01 -0700

    Django key removed

--- AngulardjangoApi/settings.py
+++ AngulardjangoApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-@t$9ob1di5(&zzo5*!yrunhhragciq*%wvlb@@2(9eebe%062j'
+
 SECRET_KEY = os.environ.get('BUGTRACKER')
 
 # SECURITY WARNING: don't run with debug turned on in production!
